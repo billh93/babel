f359fac1f7d287ce8b0be6be8b9e46e8d0962999
Bill Hinostroza <billh93@gmail.com>
2017-05-26 21:59:29 -0500

    feat: Created item.html. Finished implementing half of template.

--- client/lib/init.js
+++ client/lib/init.js
@@ -3,6 +3,6 @@
       $(".dropdown-button").dropdown();
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.carousel').carousel();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
