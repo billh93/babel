7b2b3a594f02f23bf6a19603a2abbf4a7a9e2715
Bill Hinostroza <billh93@gmail.com>
2017-05-27 15:35:24 -0500

    feat: Implemented description section for item.html

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,8 +1,12 @@
-(function($){
-  $(function(){
-      $(".dropdown-button").dropdown();
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.carousel').carousel();
-  }); // end of document ready
+(function($) {
+    $(function() {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.carousel').carousel();
+        $('.dropdown-button').dropdown({
+            constrainWidth: true, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            belowOrigin: true, // Displays dropdown below the button
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
